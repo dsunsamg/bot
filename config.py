eeb0448154e1cddbbbe41f2bd1001b0dada244f6
dsunsamg <60174794+dsunsamg@users.noreply.github.com>
2025-06-25 00:21:55 -0700

    Update config.py

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-CLAUDE_API_KEY = os.getenv("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")
+CLAUDE_API_KEY = os.getenv("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")
